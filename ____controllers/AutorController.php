a15f62c601efb5e76dd1586d53aec332e1850a61
Gabriel Coelho <gabrielroberto099@gmail.com>
2023-04-02 22:41:23 -0400

    new version

---
+++ ____controllers/AutorController.php
@@ -0,0 +1,43 @@
+<?php
+  class AutorController extends Controller {
+    function editar($id) {
+      $model = new Autor();
+      $autor = $model->getById($id);
+      $this->view('frmAutor', compact('autor'));
+    }
+
+    function listar() {
+      $model = new Autor();
+      $autores = $model->read();
+      $this->view('listagemAutor', compact('autores'));
+    }
+
+    function novo() {
+      $autor = array();
+      $autor['id'] = 0;
+      $autor['nome'] = "";
+      $autor['email'] = "";
+      $this->view("frmAutor", compact('autor'));
+    }
+
+    function salvar() {
+      $autor = array();
+      $autor['id'] = $_POST['id'];
+      $autor['nome'] = $_POST['nome'];
+      $autor['email'] = $_POST['email'];
+      $model = new Autor();
+      if ($autor['id'] == 0) {
+        $model->create($autor);
+      } else {
+        $model->update($autor);
+      }
+      $this->redirect("autor/listar");
+    }
+
+    function excluir($id) {
+      $model = new Autor();
+      $model->delete($id);
+      $this->redirect('autor/listar');
+    }
+  }
+ ?>
